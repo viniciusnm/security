0d9e1c8875e9af57118c5b3d0cd512b1df01554d
viniciusnm <viniciusnm@users.noreply.github.com>
2021-10-08 14:24:41 -0300

    Update Security.php

--- Security.php
+++ Security.php
@@ -1,2 +1,28 @@
 <?php
-//
+
+/**
+ * Class Security
+ * @package App\GeraDoc
+ */
+class Security
+{
+    private $encryptionIV = "1234567891011121";
+    private $key = "???";
+    private $ciphering = "AES-128-CTR";
+
+    public function encrypt($string)
+    {
+        return openssl_encrypt($string, $this->ciphering,
+            $this->key, 0, $this->encryptionIV);
+    }
+
+    public function decript($encryption)
+    {
+        return openssl_decrypt($encryption, $this->ciphering,
+            $this->key, 0, $this->encryptionIV);
+    }
+}
+
+$security = new Security();
+echo $security->encrypt("");
+echo $security->decript("");
